a49aeee6bd7b55967f1f33b7b383a97ccad2399e
Corrie Zhao <hfcorriez@gmail.com>
2012-02-08 00:14:15 +0800

    change left OMni to Omni

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,10 +4,10 @@ require dirname(__DIR__) . '/omni.php';
 
 define('APP_DIR', __DIR__);
 
-\OMni\Event::init(include('event.php'));
-\OMni\Event::on(\Omni\EVENT_INIT, function(){var_dump('init.');});
+\Omni\Event::init(include('event.php'));
+\Omni\Event::on(\Omni\EVENT_INIT, function(){var_dump('init.');});
 
-\OMni\App::init(include('config.php'));
-\OMni\App::run();
+\Omni\App::init(include('config.php'));
+\Omni\App::run();
 
-\OMni\Event::on(\Omni\EVENT_SHUTDOWN, function(){var_dump('shutdown at end.');});
+\Omni\Event::on(\Omni\EVENT_SHUTDOWN, function(){var_dump('shutdown at end.');});
