224001532ae2d3d56ccc4b7b61d047a22fb2a0dd
Corrie Zhao <hfcorriez@gmail.com>
2012-06-16 01:54:22 +0800

    Simple easy model for Omni (No database related.)

--- example/classes/controller/front/index.php
+++ example/classes/controller/front/index.php
@@ -8,10 +8,5 @@ class Index extends \Controller\Front
     {
         $this->_body = new \View('index');
         $this->_body->set(array('version' => \VERSION));
-
-        /*$test = \Model\Test::load(1);
-        $test->title = 'ddd';
-
-        var_dump($test->isChanged());*/
     }
 }
--- framework.php
+++ framework.php
@@ -393,17 +393,110 @@ abstract class Event
 abstract class Model
 {
     /**
-     * factory a model
-     *
-     * @static
-     *
-     * @param $model
-     *
-     * @return mixed
+     * @var \Database
      */
-    public static function factory($model)
+    protected $_data;
+
+    /**
+     * Construct new model
+     *
+     * @param array $data
+     */
+    public function __construct(array $data = array())
     {
-        return new $model;
+        foreach ($data as $key => $value) {
+            if ($this->__isValidProperty($key))
+            {
+                $this->{$key} = $value;
+                $this->_data[$key] = $value;
+            }
+        }
+    }
+
+    /**
+     * Save current values
+     *
+     * @return
+     */
+    public function save()
+    {
+        if (!$this->isChanged()) return;
+        $this->_data = $this->getChanged() + $this->_data;
+    }
+
+    /**
+     * Get changed values
+     *
+     * @return array
+     */
+    public function getChanged()
+    {
+        $changed = array();
+        foreach ($this->_data as $key => $value) {
+            if ($this->{$key} !== $value) $changed[$key] = $this->$key;
+        }
+        return $changed;
+    }
+
+    /**
+     * Check if changed
+     *
+     * @return bool
+     */
+    public function isChanged()
+    {
+        foreach ($this->_data as $key => $value) {
+            if ($this->{$key} !== $value) return true;
+        }
+        return false;
+    }
+
+    /**
+     * Export as array
+     *
+     * @return array
+     */
+    public function getAsArray()
+    {
+        $data = array();
+        foreach ($this->_data as $key => $value) {
+            $data[$key] = $this->{$key};
+        }
+        return $data;
+    }
+
+    /**
+     * Forbidden set non-exists property
+     *
+     * @param $key
+     * @param string $value
+     * @throws Exception
+     */
+    public function __set($key, $value = null)
+    {
+        throw new Exception(__CLASS__ . " has no property named '$key'.");
+    }
+
+    /**
+     * Forbidden get non-exists property
+     *
+     * @param $key
+     * @throws Exception
+     */
+    public function __get($key)
+    {
+        throw new Exception(__CLASS__ . " has no property named '$key'.");
+    }
+
+    /**
+     * Valida property name
+     *
+     * @param $key
+     * @return bool
+     */
+    private function __isValidProperty($key)
+    {
+        return $key{0} != '_' && property_exists($this, $key);
     }
 }
 
