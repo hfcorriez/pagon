2a4d6783b65e5da5888e5929bd61ade39d049f99
hfcorriez <hfcorriez@gmail.com>
2012-10-08 19:15:15 +0800

    Some improvements for pretty exception

--- src/OmniApp/Middleware/PrettyException.php
+++ src/OmniApp/Middleware/PrettyException.php
@@ -9,9 +9,7 @@ class PrettyException extends \OmniApp\Middleware
     public function call()
     {
         try {
-            ob_start();
             $this->next();
-            echo ob_get_clean();
         } catch (\Exception $e) {
             ob_clean();
             App::render(__DIR__ . '/views/error.php', array(
