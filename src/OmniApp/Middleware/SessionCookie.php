eab77ea5d93b19f933c5844b070955cc8187e2e9
hfcorriez <hfcorriez@gmail.com>
2012-11-04 20:58:18 +0800

    SessionCookie use session handler

--- src/OmniApp/Middleware/SessionCookie.php
+++ src/OmniApp/Middleware/SessionCookie.php
@@ -6,22 +6,77 @@ use OmniApp\Middleware;
 
 class SessionCookie extends Middleware
 {
-    public function call($option = array())
+    protected $options = array();
+
+    public function __construct($options = array())
     {
-        $option = $option + array(
+        $this->options = $options + array(
             'name' => 'sessions',
         );
 
+        ini_set('session.use_cookies', 0);
+        session_cache_limiter(false);
+        session_set_save_handler(
+            array($this, 'open'),
+            array($this, 'close'),
+            array($this, 'read'),
+            array($this, 'write'),
+            array($this, 'destroy'),
+            array($this, 'gc')
+        );
+    }
+
+    public function call($option = array())
+    {
+        if (session_id() === '') {
+            session_start();
+        }
+
         $req = $this->input->env();
-        if ($_sessions = $this->input->cookie($option['name'])) {
+        if ($_sessions = $this->input->cookie($this->options['name'])) {
             $_sessions = $req['sessions'] = (array)$_sessions;
         }
 
         $this->next();
 
         if ($req['sessions'] != $_sessions) {
-            $this->output->cookie($option['name'], $req['sessions'], array('sign' => true));
+            $this->output->cookie($this->options['name'], $req['sessions'], array('sign' => true));
             $req['sessions'] = array();
         }
+        session_destroy();
+    }
+
+    /*--------------------
+    * Session Handlers
+    ---------------------*/
+
+    public function open($path, $name)
+    {
+        return true;
+    }
+
+    public function close()
+    {
+        return true;
+    }
+
+    public function read($id)
+    {
+        return '';
+    }
+
+    public function write($id, $data)
+    {
+        return true;
+    }
+
+    public function destroy($id)
+    {
+        return true;
+    }
+
+    public function gc($lifetime)
+    {
+        return true;
     }
 }
