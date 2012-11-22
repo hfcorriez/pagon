e73ba373b617aa977a3c1a3adaa6e3bf52703617
hfcorriez <hfcorriez@gmail.com>
2012-11-22 17:06:20 +0800

    Add MethodOverride Middleware supported

---
+++ src/OmniApp/Middleware/MethodOverride.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace OmniApp\Middleware;
+
+class MethodOverride extends \OmniApp\Middleware
+{
+    protected $options = array(
+        'method' => '_METHOD'
+    );
+
+    public function __construct($options = array())
+    {
+        $this->options = $options + $this->options;
+    }
+
+    public function call()
+    {
+        $env = $this->input->env();
+
+        if (isset($env['X_HTTP_METHOD_OVERRIDE'])) {
+            $env['REQUEST_METHOD'] = strtoupper($env['X_HTTP_METHOD_OVERRIDE']);
+        } elseif (isset($env['REQUEST_METHOD']) && $env['REQUEST_METHOD'] === 'POST') {
+            if ($method = $_POST[$this->options['method']]) {
+                $env['original_request_method'] = $env['REQUEST_METHOD'];
+                $env['REQUEST_METHOD'] = strtoupper($method);
+            }
+        }
+
+        $this->next();
+    }
+
+}
