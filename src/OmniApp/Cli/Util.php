ea1983eaf0b844d11c9444442f03cb3d69a9427a
hfcorriez <hfcorriez@gmail.com>
2012-11-22 18:40:11 +0800

    Remove Cli\Util::options

--- src/OmniApp/Cli/Util.php
+++ src/OmniApp/Cli/Util.php
@@ -5,39 +5,6 @@ namespace OmniApp\Cli;
 class Util
 {
     /**
-     * Returns one or more command-line options. Options are specified using
-     * standard CLI syntax:
-     *     php index.php --username=john.smith --password=secret --var="some value with spaces"
-     *     // Get the values of "username" and "password"
-     *     $auth = CLI::options('username', 'password');
-     *
-     * @param   string  option name
-     * @param   ...
-     * @return  array
-     */
-    public static function options()
-    {
-        $options = func_get_args();
-        $values = array();
-
-        for ($i = 1; $i < $_SERVER['argc']; $i++) {
-            if (!isset($_SERVER['argv'][$i])) break;
-
-            $opt = $_SERVER['argv'][$i];
-            if (substr($opt, 0, 2) !== '--') continue;
-            $opt = substr($opt, 2);
-
-            if (strpos($opt, '=')) {
-                list ($opt, $value) = explode('=', $opt, 2);
-            } else $value = NULL;
-
-            if (in_array($opt, $options)) $values[$opt] = $value;
-        }
-
-        return $values;
-    }
-
-    /**
      * Color output text for the CLI
      *
      * @param string      $text       to color
