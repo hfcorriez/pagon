17686bc4ebcd0f7788624d1a2375c7d6e802e6e3
hfcorriez <hfcorriez@gmail.com>
2013-02-27 21:45:01 +0800

    Add background colors of cli support

--- lib/Pagon/Utility/Cli.php
+++ lib/Pagon/Utility/Cli.php
@@ -15,9 +15,9 @@ class Cli
      */
     public static function colorize($text, $color, $bold = false, $bg_color = 'default')
     {
-        $colors = array_flip(array(30 => 'gray', 'red', 'green', 'yellow', 'blue', 'purple', 'cyan', 'white', 'default'));
+        $colors = array_flip(array(30 => 'gray', 'red', 'green', 'yellow', 'blue', 'purple', 'cyan', 'white'));
 
-        $bg_colors = array_flip(array(40 => 'gray', 'red', 'green', 'yellow', 'blue', 'purple', 'cyan', 'white', 'default'));
+        $bg_colors = array_flip(array(40 => 'gray', 'red', 'green', 'yellow', 'blue', 'purple', 'cyan', 'white', 49 => 'default'));
 
         return "\033[" . ($bold ? '1' : '0') . ';' . $colors[$color] . ';' . $bg_colors[$bg_color] . "m$text\033[0m";
     }
