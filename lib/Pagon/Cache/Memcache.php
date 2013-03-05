59bef5885bb2b3fc85ec666d056b06c7e504fa50
hfcorriez <hfcorriez@gmail.com>
2013-03-05 18:40:49 +0800

    Implements cache interface of Memcache, Memcached, Redis

---
+++ lib/Pagon/Cache/Memcache.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Pagon\Cache;
+
+class Redis
+{
+    public $cache;
+
+    protected $options = array(
+        'host'    => 'localhost',
+        'port'    => '6379',
+        'timeout' => 1
+    );
+
+    /**
+     * Init cache
+     *
+     * @param array $options
+     */
+    public function __construct(array $options = array())
+    {
+        $this->options = $options + $this->options;
+
+        $this->cache = new \Memcache();
+        $this->cache->connect($this->options['host'], $this->options['port'], $this->options['timeout']);
+    }
+
+
+    /**
+     * Get value
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function get($key)
+    {
+        return $this->cache->get($key);
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $key
+     * @param string $value
+     * @param int    $timeout
+     * @return bool
+     */
+    public function set($key, $value, $timeout = 0)
+    {
+        return $this->cache->set($key, $value, $timeout);
+    }
+
+    /**
+     * Delete key
+     *
+     * @param string $key
+     * @return bool
+     */
+    public function delete($key)
+    {
+        return $this->cache->delete($key);
+    }
+}
---
+++ lib/Pagon/Cache/Memcached.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Pagon\Cache;
+
+class Redis
+{
+    public $cache;
+
+    protected $options = array(array(
+        'host'   => 'localhost',
+        'port'   => '6379',
+        'weight' => 0
+    ));
+
+    /**
+     * Init cache
+     *
+     * @param array $options
+     */
+    public function __construct(array $options = array())
+    {
+        if (isset($options[0]) && is_array($options[0])) {
+            $this->options = $options + $this->options;
+        } else {
+            $this->options = array($options + $this->options[0]);
+        }
+
+        $this->cache = new \Memcached();
+        foreach ($this->options as $server) {
+            $this->cache->addServer($server['host'], $server['port'], $server['weight']);
+        }
+    }
+
+    /**
+     * Get value
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function get($key)
+    {
+        return $this->cache->get($key);
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $key
+     * @param string $value
+     * @param int    $timeout
+     * @return bool
+     */
+    public function set($key, $value, $timeout = 0)
+    {
+        return $this->cache->set($key, $value, $timeout);
+    }
+
+    /**
+     * Delete key
+     *
+     * @param string $key
+     * @return bool
+     */
+    public function delete($key)
+    {
+        return $this->cache->delete($key);
+    }
+}
