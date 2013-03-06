0c50ad9ff2744d810b555f985ea266ea760c0c60
hfcorriez <hfcorriez@gmail.com>
2013-03-06 22:06:05 +0800

    Implement the Jade View Engine

---
+++ lib/Pagon/Engine/Jade.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Pagon\Engine;
+
+use Everzet\Jade\Jade as Jader;
+use Everzet\Jade\Dumper\PHPDumper;
+use Everzet\Jade\Parser;
+use Everzet\Jade\Lexer\Lexer;
+
+class Jade
+{
+    /**
+     * @var array Default options
+     */
+    protected $options = array(
+        'compile_dir' => '/tmp'
+    );
+
+    protected $engine;
+
+    /**
+     * Init the engine
+     *
+     * @param array $options
+     */
+    public function __construct(array $options = array())
+    {
+        $this->options = $options + $this->options;
+
+        $dumper = new PHPDumper();
+        $parser = new Parser(new Lexer());
+
+        $this->engine = new Jader($parser, $dumper, $this->options['compile_dir']);
+    }
+
+    /**
+     * Implements the render
+     *
+     * @param string $path
+     * @param array  $data
+     * @param string $dir
+     * @return string
+     */
+    public function render($path, $data, $dir)
+    {
+        $file = $this->engine->cache($dir . $path);
+
+        ob_start();
+        if ($data) {
+            extract((array)$data);
+        }
+        include($file);
+
+        return ob_get_clean();
+    }
+}
