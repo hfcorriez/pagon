979b0bf739a06de765c03fe10851207772841111
hfcorriez <hfcorriez@gmail.com>
2013-03-18 21:45:46 +0800

    Add cli::exec and cli::pipe Api to run the shell

--- lib/Pagon/Helper/Cli.php
+++ lib/Pagon/Helper/Cli.php
@@ -84,4 +84,64 @@ class Cli
 
         return $ret;
     }
+
+    /**
+     * Execute the command
+     *
+     * @param string $cmd
+     * @param string $stdout
+     * @param string $stderr
+     * @param int    $timeout
+     * @return int
+     */
+    public static function exec($cmd, &$stdout, &$stderr, $timeout = 3600)
+    {
+        if ($timeout <= 0) $timeout = 3600;
+        $descriptors = array
+        (
+            1 => array("pipe", "w"),
+            2 => array("pipe", "w")
+        );
+
+        $stdout = $stderr = $status = null;
+        $process = proc_open($cmd, $descriptors, $pipes);
+
+        $time_end = time() + $timeout;
+        if (is_resource($process)) {
+            do {
+                $time_left = $time_end - time();
+                $read = array($pipes[1]);
+                stream_select($read, $write = NULL, $exceptions = NULL, $time_left, NULL);
+                $stdout .= fread($pipes[1], 2048);
+            } while (!feof($pipes[1]) && $time_left > 0);
+            fclose($pipes[1]);
+
+            if ($time_left <= 0) {
+                proc_terminate($process);
+                $stderr = 'process terminated for timeout.';
+                return -1;
+            }
+
+            while (!feof($pipes[2])) {
+                $stderr .= fread($pipes[2], 2048);
+            }
+            fclose($pipes[2]);
+
+            $status = proc_close($process);
+        }
+
+        return $status;
+    }
+
+    /**
+     * Execute the command with pipe
+     *
+     * @static
+     * @param $cmd
+     * @return int
+     */
+    public static function pipe($cmd)
+    {
+        return pclose(popen($cmd . ' &', 'r'));
+    }
 }
