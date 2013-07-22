8e46069d0b511abbd70d29360f6d3b07168e3f29
hfcorriez <hfcorriez@gmail.com>
2013-07-22 16:28:07 +0800

    Add CRSF middleware for safe check

---
+++ lib/Pagon/Middleware/CRSF.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Pagon\Middleware;
+
+use Pagon\Middleware;
+
+class CRSF extends Middleware
+{
+    // Some options
+    protected $options = array(
+        'form'        => true,
+        'refer'       => true,
+        'form_name'   => '__ct',
+        'cookie_name' => '__st',
+        'sign'        => 'fu*kCrSF',
+        'expires'     => 3600
+    );
+
+    /**
+     * Call
+     *
+     * @return bool|void
+     */
+    public function call()
+    {
+        if ($this->input->isPost()) {
+            $token = $this->input->data($this->options['form_name']);
+            if (!$token
+                || !($value = $this->decodeToken($token))
+                || $value[0] != $this->options['sign']
+                || ($value[1] + $this->options['expires'] < time())
+            ) {
+                if (isset($this->options['callback'])) {
+                    $this->options['callback']($this->input, $this->output);
+                } else {
+                    $this->output->status = 403;
+                    $this->output->end('<html><head><title></title></head><body><h2>Security Check Forbidden!</h2></body></html>');
+                }
+                return;
+            }
+        }
+
+        $this->next();
+
+        // Check if buffer start
+        if ($this->app->buffer) {
+            $body = ob_get_clean();
+        } else {
+            $body = $this->output->body;
+        }
+
+        if (strpos($body, '</form>')) {
+            $this->output->body = preg_replace('/.*?<\/form>/', '<input type="hidden" name="' . $this->options['form_name'] . '" value="' . $this->encodeToken() . '" /></form>', $body);
+        } else {
+            $this->output->body = $body;
+        }
+    }
+
+    protected function decodeToken($token)
+    {
+        if ($value = $this->app->cryptor->decrypt($token)) {
+            return explode('||', $value);
+        }
+        return false;
+    }
+
+    protected function encodeToken()
+    {
+        return $this->app->cryptor->encrypt($this->options['sign'] . '||' . time());
+    }
+}
