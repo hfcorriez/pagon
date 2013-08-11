25a590299549dd66599ab782e4e7f46435f8a963
hfcorriez <hfcorriez@gmail.com>
2013-08-11 16:28:44 +0800

    Improve PageCache and remove encode config

--- lib/Pagon/Middleware/PageCache.php
+++ lib/Pagon/Middleware/PageCache.php
@@ -12,65 +12,59 @@ class PageCache extends Middleware
         'key'     => false,
         'cache'   => false,
         'hash'    => true,
-        'encode'  => 'json',
         'timeout' => 0
     );
 
     public function call()
     {
+        // Check cache provider and input method
         if (!$this->options['cache'] || !$this->input->is("get")) {
             $this->next();
             return;
         }
 
         if ($this->options['domain']) {
+            // If generate key with domain
             $key = $this->input->url();
         } elseif ($this->options['key']) {
+            // Support create key dynamic
             if ($this->options['key'] instanceof \Closure) {
-                $key = call_user_func($this->options['key']);
+                $key = call_user_func($this->options['key'], $this->input, $this->output);
             } else {
                 $key = $this->options['key'];
             }
         } else {
+            // Create key only with uri
             $key = $this->input->uri();
         }
 
         if ($this->options['hash']) {
+            // Hash the key
             $key = sha1($this->options['hash']);
         }
 
+        // Add prefix for the key
         $key = $this->options['prefix'] . $key;
 
         /** @var $cache \Pagon\Cache */
         $cache = $this->options['cache'];
 
         if ($page = $cache->get($key)) {
-            switch ($this->options['encode']) {
-                case 'json':
-                    $page = json_decode($page, true);
-                    break;
-                default:
-                    $page = unserialize($page);
-            }
+            // Try to get the page cacheF
+            $page = json_decode($page, true);
             $this->output->header($page['header']);
             $this->output->end($page['body']);
             return;
         }
 
+        // Next
         $this->next();
 
         $page = array();
         $page['header'] = $this->output->header();
         $page['body'] = $this->output->body();
 
-        switch ($this->options['encode']) {
-            case 'json' :
-                $page = json_encode($page);
-                break;
-            default:
-                $page = serialize($page);
-        }
-
-        $cache->set($key, $page, $this->options['timeout']);
+        // Save data to cache
+        $cache->set($key, json_encode($page), $this->options['timeout']);
     }
 }
