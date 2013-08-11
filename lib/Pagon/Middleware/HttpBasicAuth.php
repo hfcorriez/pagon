da37deaee5f93799c4045e3cc96120f352323812
hfcorriez <hfcorriez@gmail.com>
2013-08-11 16:43:24 +0800

    Add comments for HttpBasicAuth

--- lib/Pagon/Middleware/HttpBasicAuth.php
+++ lib/Pagon/Middleware/HttpBasicAuth.php
@@ -23,13 +23,19 @@ class HttpBasicAuth extends Middleware
 
     public function call()
     {
+        // Inject the error type
         $this->app->errors['401'] = array(401, 'Authorization Required!');
+
+        // Try to get username and password
         $username = isset($_SERVER['PHP_AUTH_USER']) ? $_SERVER['PHP_AUTH_USER'] : null;
         $password = isset($_SERVER['PHP_AUTH_PW']) ? $_SERVER['PHP_AUTH_PW'] : null;
 
+        // Check username and password
         if (empty($username) || empty($password) || !$this->input->session($this->options['session_name'])) {
+            // Set session
             $this->input->session($this->options['session_name'], 1);
 
+            // Set forbidden
             $this->output->header("WWW-Authenticate", "Basic realm=\"{$this->options['realm']}\"");
             $this->app->handleError(401);
         } else {
@@ -37,8 +43,10 @@ class HttpBasicAuth extends Middleware
                 && call_user_func($this->options['callback'], $username, $password)
                 || $this->options['user'] == $username && $this->options['pass'] == $password
             ) {
+                // If auth pass
                 $this->next();
             } else {
+                // Else redirect to current url
                 $this->input->session($this->options['session_name'], 0);
                 $this->output->header("Location", $this->input->url())->end();
             }
