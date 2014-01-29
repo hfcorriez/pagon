3a81a44fff25e52866d68b40b12f1ca03e6e478e
hfcorriez <hfcorriez@gmail.com>
2014-01-29 11:41:29 +0800

    Remove jade engine from core

--- pack/pagon.core.php
+++ pack/pagon.core.php
@@ -3080,53 +3080,6 @@ class Output extends EventEmitter
 }
 }
 
-namespace Pagon\Engine {
-
-use Everzet\Jade\Jade as Jader;
-use Everzet\Jade\Dumper\PHPDumper;
-use Everzet\Jade\Parser;
-use Everzet\Jade\Lexer\Lexer;
-
-
-
-class Jade
-{
-    protected $options = array(
-        'compile_dir' => '/tmp'
-    );
-
-    protected $engine;
-
-    public function __construct(array $options = array())
-    {
-        if (!class_exists('\Everzet\Jade\Jade')) {
-            throw new \RuntimeException("Use Jade engine need `composer update` to install or include manually.");
-        }
-
-        $this->options = $options + $this->options;
-
-        $dumper = new PHPDumper();
-        $parser = new Parser(new Lexer());
-
-        $this->engine = new Jader($parser, $dumper, $this->options['compile_dir']);
-    }
-
-    public function render($path, $data, $dir)
-    {
-        $file = $this->engine->cache($dir . $path);
-
-        ob_start();
-        if ($data) {
-            extract((array)$data);
-        }
-        include($file);
-
-        return ob_get_clean();
-    }
-}
-
-}
-
 namespace Pagon\Exception {
 
 
